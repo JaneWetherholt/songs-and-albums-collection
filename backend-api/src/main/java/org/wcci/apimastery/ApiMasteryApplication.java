56c986a555678db71d3509357854d0ced15f0efd
Jane Wetherholt <janewetherholt@gmail.com>
2020-12-19 20:00:20 -0500

    Initial commit

---
+++ backend-api/src/main/java/org/wcci/apimastery/ApiMasteryApplication.java
@@ -0,0 +1,12 @@
+package org.wcci.apimastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMasteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMasteryApplication.class, args);
+    }
+}
---
+++ frontend-spa/src/js/sampleAlbumJSON.js
@@ -0,0 +1,81 @@
+const allAlbums = [
+  {
+    "title": "Back in Black",
+    "artist": "AC/DC",
+    "rating": 5,
+    "image": "img",
+    "recordLabel": "Back in Black",
+    "id": 1,
+    "songs": [
+      {
+        "id": 2,
+        "title": "Hells bells",
+        "duration": 5,
+        "link": "link",
+        "comments": [
+          {
+            "id": 7,
+            "commentContent": "pretty iconic song !"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "title": "Shoot to Thrill",
+        "duration": 5,
+        "link": "link",
+        "comments": []
+      },
+      {
+        "id": 4,
+        "title": "What Do You Do For Money Honey",
+        "duration": 5,
+        "link": "link",
+        "comments": []
+      },
+      {
+        "id": 5,
+        "title": "Let Me Put My Love Into You",
+        "duration": 4,
+        "link": "link",
+        "comments": []
+      },
+      {
+        "id": 6,
+        "title": "Have a Drink On Me",
+        "duration": 4,
+        "link": "link",
+        "comments": []
+      }
+    ]
+  },
+  {
+    "title": "Greatest Hits",
+    "artist": "Eagles",
+    "rating": 10,
+    "image": "img",
+    "recordLabel": "Eagles",
+    "id": 8,
+    "songs": [
+      {
+        "id": 9,
+        "title": "Take it Easy",
+        "duration": 3,
+        "link": "link",
+        "comments": [
+          {
+            "id": 10,
+            "commentContent": "this is a test"
+          },
+          {
+            "id": 11,
+            "commentContent": "this is a test 3"
+          }
+        ]
+      }
+    ]
+  }
+]
+export {
+  allAlbums
+}
